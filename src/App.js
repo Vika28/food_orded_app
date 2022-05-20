2a400fb5886ca9a9c25708bb2994cadaf5ea8c82
Viktoriia Nakoryk <Viktoriia_Nakoryk@epam.com>
2022-05-20 16:24:23 +0200

    added basic markup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Header from './components/Layout/Header';
+import { Fragment } from "react";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Fragment>
+        <Cart />
+        <Header/>
+        <main>
+            <Meals />
+        </main>
+    </Fragment>
   );
 }
 
