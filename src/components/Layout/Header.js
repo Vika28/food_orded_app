3989e3b7f8b8aa8a27f816374e2087bbd70d9cc1
Viktoriia Nakoryk <Viktoriia_Nakoryk@epam.com>
2022-05-20 17:58:34 +0200

    finished app

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
         <Fragment>
             <header className={styles.header}>
                 <h1>React Meals</h1>
-                <HeaderCartButton />
+                <HeaderCartButton onClick={props.onShowCart}/>
             </header>
             <div className={styles['main-image']}>
                 <img src={meals} alt=""/>
